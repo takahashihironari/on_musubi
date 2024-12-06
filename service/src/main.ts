61239d53ef697ac0e32eaf312658be78f1501a28
takahashihironari <hironari10251128@gmail.com>
2024-12-06 20:03:38 +0900

    ログ追加

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // ログレベルの設定
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
+
+  // グローバルロガーの設定
+  const logger = new Logger('Bootstrap');
+  logger.log('アプリケーションを起動中');
+
   await app.listen(4000);
+  logger.log(`アプリケーションは http://localhost:4000 で起動しました`);
 }
 bootstrap();
